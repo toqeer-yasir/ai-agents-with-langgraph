634ffbdf52f3ca4209a32fa3d51964946ccea1f4
toqeeryasir <miantoqeeryasir0@gmail.com>
2025-09-12 12:23:28 +0500

    tools integration

--- agentic_chatbot/chatbot_backend.py
+++ agentic_chatbot/chatbot_backend.py
@@ -1,18 +1,22 @@
-from langgraph.graph import StateGraph, START, END
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import AIMessage
+# library to create state_class
 from typing import TypedDict, Annotated
-from dotenv import load_dotenv
+from langchain_core.messages import AIMessage
 from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
 # from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
 import os
+# libraries to add tools:
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools import DuckDuckGoSearchRun
 
 
 load_dotenv()
 
-model = ChatOpenAI(
+llm = ChatOpenAI(
     # model="mistralai/mistral-7b-instruct"
     model="mistralai/Mistral-7B-Instruct-v0.1",
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
@@ -20,21 +24,30 @@ model = ChatOpenAI(
     temperature=0.1
     )
 
+search_tool = DuckDuckGoSearchRun(region= 'us-en')
+tools = [search_tool]
+llm_with_tools = llm.bind_tools(tools=tools)
+
 class ChatState(TypedDict):
     messages: Annotated[list, add_messages]
 
 def chat_node(state: ChatState):
     message = state['messages']
-    response = model.invoke(message)
+    response = llm_with_tools.invoke(message)
     return {'messages': [AIMessage(content=response.content)]}
+
+tool_node = ToolNode(tools=tools)
 
 # define graph:
 graph = StateGraph(ChatState)
 # add nodes:
 graph.add_node('chat_node', chat_node)
+graph.add_node('tools', tool_node)
 # add edges:
 graph.add_edge(START, 'chat_node')
-graph.add_edge('chat_node', END)
+graph.add_conditional_edges('chat_node', tools_condition)
+graph.add_edge('tools', 'chat_node')
+
 
 CONN = sqlite3.connect(database='database/chatbot.db', check_same_thread=False)
 
