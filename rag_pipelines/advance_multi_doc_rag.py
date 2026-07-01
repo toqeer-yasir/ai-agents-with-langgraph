8b39d91709961ee98efbe7ac44d0e65a377c2ccb
toqeer-yasir <toqeeryasir01@gmail.com>
2026-07-01 11:41:44 +0500

    updates code to use langchain_chroma insted of separate chroma lib

--- rag_pipelines/advance_multi_doc_rag.py
+++ rag_pipelines/advance_multi_doc_rag.py
@@ -9,27 +9,26 @@ from langchain_community.document_loaders import (
 )
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-import chromadb
-from chromadb.utils import embedding_functions
+from langchain_chroma import Chroma
 
 
 class Rag:
     def __init__(self, db_path: str = "./chroma_db", collection_name: str = "rag_docs"):
         """Initializing or creating db, creating splitter object"""
-        client = chromadb.PersistentClient(path=db_path)
-        embedding_fn = embedding_functions.DefaultEmbeddingFunction()
+        self.embedding_fn = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
         
-        self.collection = client.get_or_create_collection(
-            name = collection_name,
-            embedding_function = embedding_fn
+        self.vector_store = Chroma(
+            collection_name = collection_name,
+            embedding_function = self.embedding_fn,
+            persist_directory = db_path
         )
         
-        is_new_store = (self.collection.count() == 0)
+        is_new_store = (len(self.vector_store.get()["ids"]) == 0)
         
         if is_new_store:
             print(f"Database initialized successfully! Ready for ingestion.")
         else:
-            print(f"Successfully connected to database heaving {self.collection.count()} chunks.")
+            print(f"Successfully connected to database heaving {len(self.vector_store.get()['ids'])} chunks.")
         
         self.splitter = RecursiveCharacterTextSplitter(
         chunk_size = 500,
@@ -55,7 +54,7 @@ class Rag:
             doc_content = CSVLoader(doc_path).load()
 
         else:
-            raise TypeError(f"Uploaded file type '{doc_path}' is not suported by RAG!")
+            raise TypeError(f"Uploaded file type '{doc_path}' is not supported by RAG!")
 
         return doc_content
     
@@ -69,66 +68,65 @@ class Rag:
 
 
     def _split_documents(self, doc_content):
-        docs = self.splitter.split_documents(doc_content)
-        return docs
+        return self.splitter.split_documents(doc_content)
     
     
     def _add_documents(self, docs: list[str], doc_path):
-
-        chunks = []
         chunk_ids = []
-
         file_signature = self._calculate_file_hash(doc_path)
 
         for doc in docs:
             doc.metadata["file_signature"] = file_signature
             source_file = doc.metadata.get("source", "unknown_file")
             text_content = doc.page_content
+            
             chunk_hash = hashlib.md5(f"{source_file}_{text_content}".encode('utf-8')).hexdigest()
             chunk_ids.append(chunk_hash)
-            chunks.append(text_content)
-            
-        metadatas = [doc.metadata for doc in docs]
 
-        self.collection.add(
-            documents = chunks,
-            ids = chunk_ids,
-            metadatas=metadatas
+        self.vector_store.add_documents(
+            documents = docs,
+            ids = chunk_ids
         )
         print("Document successfully added to store.")
     
     
     def delete_documents(self, target_file_path: str):
-        self.collection.delete(
-            where={"source": target_file_path}
-        )
-        print(f"Target file {target_file_path} deleted successfully from store.")
+        all_data = self.vector_store.get(where={"source": target_file_path})
+        if all_data is None or len(all_data["ids"]) == 0:
+            print(f"No document found with source path: {target_file_path}")
+            return
+        else:
+            self.vector_store.delete(ids=all_data["ids"])
+            print(f"Deleted document with source path: {target_file_path}")
 
-    
-    
-    def retriever(self, query: str, top_k: int = 3):
-        results = self.collection.query(
-            query_texts = [query],
-            n_results = top_k
-        )
-        return results["documents"][0]
-    
+
     
     def ingest_documents(self, doc_path:str = None):
         if not doc_path:
             print("No doc path given.")
             return
+
         file_signature = self._calculate_file_hash(doc_path)
 
-        exist = self.collection.get(where={"file_signature": file_signature})
+        exist = self.vector_store.get(where={"file_signature": file_signature})
         if exist and len(exist["ids"]) > 0:
             print((f"File {os.path.basename(doc_path)} with same content has already been ingested."))
             return
+
         doc_content = self._load_documents(doc_path)
         docs = self._split_documents(doc_content)
         self._add_documents(docs, doc_path)
         print("Ingestion completed successfully.")
         return 
+
+
+    def retriever(self, query: str, top_k: int = 3):
+        results = self.vector_store.similarity_search(
+            query,
+            k = top_k
+        )
+        return results["documents"]
+
 
 my_rag = Rag()
 
