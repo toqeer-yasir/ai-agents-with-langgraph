4ee0a01280c2f3e0f244c488563ef6c654f447bd
toqeer-yasir <toqeeryasir01@gmail.com>
2026-07-08 12:23:15 +0500

    updates websocket code

--- rag_agent_with_chroma_db_and_psql_db/rag_agent.py
+++ rag_agent_with_chroma_db_and_psql_db/rag_agent.py
@@ -1,14 +1,26 @@
-import asyncio
 import os
+from contextlib import asynccontextmanager
 from typing import Annotated
+
 from dotenv import load_dotenv
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from langchain_core.messages import BaseMessage, AIMessage, HumanMessage
 from langchain_openrouter import ChatOpenRouter
-from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.graph import StateGraph, START, END
+
+from langgraph.graph import START, StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+
 from psycopg_pool import AsyncConnectionPool
+
 from typing_extensions import TypedDict
+
+from tools import get_tools
+
 
 
 load_dotenv()
@@ -17,63 +29,171 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 
-model = ChatOpenRouter(
-    model="openrouter/free",
-    api_key=OPENROUTER_API_KEY,
-    temperature=0.7,
-)
-
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
 
-async def chatbot(state: ChatState):
 
-    response = await model.ainvoke(state["messages"])
+def create_chat_node(model_with_tools):
 
-    return {
-        "messages": [response]
-    }
+    async def chat_node(state: ChatState):
+
+        response = await model_with_tools.ainvoke(
+            state["messages"]
+        )
+
+        return {
+            "messages": [response]
+        }
+
+    return chat_node
 
 
-builder = StateGraph(ChatState)
 
-builder.add_node("chatbot", chatbot)
+def build_graph(model_with_tools, tools, checkpointer):
 
-builder.add_edge(START, "chatbot")
-builder.add_edge("chatbot", END)
+    builder = StateGraph(ChatState)
+
+    chat_node = create_chat_node(model_with_tools)
+
+    builder.add_node("chatnode", chat_node)
+    builder.add_node("tools", ToolNode(tools))
+
+    builder.add_edge(START, "chatnode")
+
+    builder.add_conditional_edges(
+        "chatnode",
+        tools_condition,
+    )
+
+    builder.add_edge(
+        "tools",
+        "chatnode",
+    )
+
+    return builder.compile(
+        checkpointer=checkpointer
+    )
 
 
-async def main():
 
-    async with AsyncConnectionPool(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    print("Starting server...")
+
+    pool = AsyncConnectionPool(
         DATABASE_URL,
         min_size=4,
-        max_size=10
-    ) as pool:
+        max_size=10,
+        open=False
+    )
 
-        checkpointer = AsyncPostgresSaver(pool)
-        await checkpointer.setup()
+    await pool.open()
 
-        graph = builder.compile(checkpointer=checkpointer)
+    checkpointer = AsyncPostgresSaver(pool)
 
-        thread_id = "chat_2"
+    await checkpointer.setup()
 
-        print("\nType 'exit' to quit.\n")
+    model = ChatOpenRouter(
+        model="openrouter/free",
+        api_key=OPENROUTER_API_KEY,
+        streaming= True,
+        temperature=0.7
+    )
 
+    tools, mcp_client = await get_tools()
+    print(f"Tools Received: {len(tools)}")
+
+    model_with_tools = model.bind_tools(tools)
+
+    graph = build_graph(
+        model_with_tools=model_with_tools,
+        tools=tools,
+        checkpointer=checkpointer,
+    )
+
+    app.state.pool = pool
+    app.state.graph = graph
+    app.state.model = model
+    app.state.tools = tools
+    app.state.mcp_client = mcp_client
+
+    yield
+
+    print("Stopping server...")
+
+    await pool.close()
+
+    if hasattr(app.state, "mcp_client"):
+        await app.state.mcp_client.aclose()
+
+
+
+app = FastAPI(
+    title="Agentic RAG Backend",
+    lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.websocket("/ws/chat")
+async def websocket_chat(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
         while True:
+            data = await websocket.receive_json()
+            message = data.get("message")
+            thread_id = data.get("thread_id")
+            user_id = data.get("user_id")
+            
+            if not message or not thread_id:
+                await websocket.send_json({
+                    "type": "error",
+                    "content": "Missing message or thread_id"
+                })
+                continue
+            
+            msg = {'messages': [HumanMessage(content=message)]}
+            config = {"configurable": {'thread_id': thread_id}}
+            context={"user_id": user_id}
 
-            query = input("You : ")
+            try:
+                async for message_chunk, _ in websocket.app.state.graph.astream(
+                    msg,
+                    config= config,
+                    context= context,
+                    stream_mode= ["messages", "updates"]
+                ):
 
-            if query.lower() == "exit":
-                break
+                    if isinstance(message_chunk, AIMessage) and message_chunk.content:
+                        await websocket.send_json({
+                            "type": "content",
+                            "content": message_chunk.content
+                        })
 
-            result = await graph.ainvoke({"messages": [HumanMessage(content= query)]},
-                config={"configurable": {"thread_id": thread_id,}},)
+                await websocket.send_json({
+                    "type": "completed"
+                })
 
-            result = "\nAssistant :", result["messages"][-1].content
-            print(f"{result[0]} {result[1]}")
+            except Exception as e:
+                await websocket.send_json({
+                    "type": "error",
+                    "content": str(e)
+                })
+    
+    except WebSocketDisconnect:
+        print("Client Disconnected")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
