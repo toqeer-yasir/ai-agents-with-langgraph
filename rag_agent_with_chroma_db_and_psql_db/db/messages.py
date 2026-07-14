b82402cef19a31b537f1a1e166805ff9380da78c
toqeer-yasir <toqeeryasir01@gmail.com>
2026-07-14 14:37:58 +0500

    Rename message-ID param and add parent_message_id support

--- rag_agent_with_chroma_db_and_psql_db/db/messages.py
+++ rag_agent_with_chroma_db_and_psql_db/db/messages.py
@@ -1,7 +1,7 @@
 from uuid import UUID, uuid4
 
-async def create_message(pool, id, chat_id, user_content, assistant_content):
-    assistant_id = uuid4()
+async def create_message(pool, message_id, chat_id, user_content, assistant_content):
+    assistant_message_id = uuid4()
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
             await cur.execute(
@@ -9,7 +9,7 @@ async def create_message(pool, id, chat_id, user_content, assistant_content):
                 INSERT INTO messages(id, chat_id, role, content)
                 VALUES (%s, %s, 'user', %s)
                 """,
-                (id, chat_id, user_content)
+                (message_id, chat_id, user_content)
             )
 
             await cur.execute(
@@ -17,7 +17,7 @@ async def create_message(pool, id, chat_id, user_content, assistant_content):
                 INSERT INTO messages(id, chat_id, role, content, parent_message_id)
                 VALUES (%s, %s, 'assistant', %s, %s)
                 """,
-                (assistant_id, chat_id, assistant_content, id)
+                (assistant_message_id, chat_id, assistant_content, message_id)
             )
 
             
@@ -34,7 +34,6 @@ async def get_chat_messages(pool, chat_id):
                 role,
                 content,
                 parent_message_id,
-                created_at
                 FROM messages
                 WHERE chat_id = %s
                 ORDER BY created_at ASC;
@@ -50,7 +49,6 @@ async def get_chat_messages(pool, chat_id):
                 "role": row[1],
                 "content": row[2],
                 "parent_message_id": row[3],
-                "created_at": row[4],
             }
             for row in rows
         ]
